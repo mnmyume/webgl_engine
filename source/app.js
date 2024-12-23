43556e529fc06d02271b9323e9ee24dfbe7f2329
mnmyume <91729958+mnmyume@users.noreply.github.com>
2024-12-23 14:20:33 +0800

    `

--- source/app.js
+++ source/app.js
@@ -8,6 +8,7 @@ import FPSCounter from './fpscounter.js';
 import Time from './time.js';
 import StaticEmitter from './staticEmitter.js';
 import ParticleMaterial from './particleMaterial.js';
+import {genPos, generateCirclePos, generateCirclePosRandom} from './generatorHelper.js';
 
 import { basicVert, basicFrag, particle3dVert, particle2dVert, particleFrag } from "../shaders/output.js";
 
@@ -62,23 +63,6 @@ camera.setPosition([0, 0, 300]);
 camera.updateProjection();
 camera.updateView();
 camera.updateViewInverse();
-
-function genPos() {
-    let posArray = [];
-    const startX = -8;
-    const startY = -8;
-    const spacing = 2;
-    
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            const posX = startX + j * spacing;
-            const posY = startY + i * spacing;
-            posArray.push([posX, posY, -1.0]);
-        }
-    }
-
-    return posArray;
-}
 
 function genQuadWithUV(out, index) {
     const uvCoordinates = [
@@ -156,53 +140,6 @@ function drawSimpleQuad() {
     }
     
     requestAnimationFrame(drawSimpleQuad);
-}
-
-function generateCirclePos(numParticle, generation) { 
-    const posPixels = [];
-    const radius = 50;
-
-    for(let row = 0; row < generation; row++) {
-        const offset = 2 * Math.PI / generation * row;  // Math.random() *
-        for (let col = 0; col < numParticle; col++) {
-            const angle = 2 * Math.PI / (numParticle) * col;
-            const x = radius * Math.cos(angle + offset);
-            const y = radius * Math.sin(angle + offset);
-
-            posPixels.push(x, y, 0.0, 0.0);
-        }
-    }
-
-    return posPixels;
-}
-
-// 0--duration(1s)
-// 0 -1000ms (delta: 66.7) ==== rate:60 i++ (0-60)
-// Math.Random()*2*PI ==> x,y
-// startTime: i*66.7/1000
-function generateCirclePosRandom(numParticle, startSize, endSize) {
-    const posPixels = [];
-    const radius = 50;
-
-    for(let i = 0; i < numParticle; i++) {
-
-        // position
-        const angle = Math.random() * 2 * Math.PI; 
-        const x = Math.cos(angle) * radius;
-        const y = Math.sin(angle) * radius;
-
-        posPixels.push(x, y, 0.0, startSize);
-
-        // linearVelocity
-        const tangentX = Math.sin(angle); 
-        const tangentY = -Math.cos(angle);  
-        const speed = (angle / (2 * Math.PI)) * 60;
-
-        posPixels.push(tangentX * speed, tangentY * speed, 0.0, endSize);
-
-    }
-
-    return posPixels;
 }
 
 function initParticles() {
