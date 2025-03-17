6d1f48faedcb2e492a805ca47e5dd4dd0d7ee496
Chopin <dourabbit@gmail.com>
2025-03-17 21:31:38 +0800

    ~

--- shaders/glsl/solver-frag.glsl
+++ shaders/glsl/solver-frag.glsl
@@ -194,7 +194,7 @@ void main() {
     }
 
 
-    gl_FragData[0] = vec4(pos, 15);
+    gl_FragData[0] = vec4(0.0,particleID,0.0, 15);
     gl_FragData[1] = vec4(vel, 1);
     gl_FragData[2] = vec4(0.0, 1.0, 0.0, 1.0);
     gl_FragData[3] = vec4(1.0, 0.0, 0.0,1.0);
